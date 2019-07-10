8002f1ad59e3bc5790decfd0958ee89b2621b497
Osa <osaimola@gmail.om>
2019-07-10 07:52:11 +0100

    add new components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <!-- Add a TrackList component -->
+      </div>);
+  }
+}
