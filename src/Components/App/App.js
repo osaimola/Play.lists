2649f38e4ae8315c42ed6cfec7f8d0bc03d8f36c
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-10 19:58:06 +0100

    add child components to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './App.css';
+import {SearchBar} from "../SearchBar/SearchBar";
+import {SearchResults} from "../SearchResults/SearchResults";
+import {Playlist} from "../Playlist/Playlist";
 
-function App() {
-  return(
-  <div>
-    <h1>Ja<span className="highlight">mmm</span>ing</h1>
-    <div className="App">
-      <!-- Add a SearchBar component -->
-      <div className="App-playlist">
-        <!-- Add a SearchResults component -->
-        <!-- Add a Playlist component -->
+const track = {id: 5, name:"Gully", artist:"Santi", album:"Mandy"};
+const listOfTracks = [track, track, track];
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: listOfTracks,
+      playListName: "Wavy Songs",
+      playListTracks: listOfTracks
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playListName={this.state.playListName}
+                      playListTracks={this.state.playListTracks} />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 
 export default App;
